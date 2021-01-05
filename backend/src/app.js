43aa534902095260f25b7249c531e2b646bae4ad
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-01-05 22:40:19 +0530

    daypoint reset done

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
-
+const User = require("./models/user.js")
 
 //My routes
 const authRoutes = require("./routes/auth");
@@ -31,6 +31,26 @@ app.use("/game", dataRoutes);
 app.use("/leaderboard", rankRoutes);
 app.use("/updatescores",scoreRoutes);
 
+function scheduleReset() {
+    // get current time
+    let reset = new Date();
+    // update the Hours, mins, secs to the 24th hour (which is when the next day starts)
+    reset.setHours(24, 0, 0, 0);
+    // calc amount of time until restart
+    let t = reset.getTime() - Date.now();
+    setTimeout(function() {
+        // reset variable
+        User.updateMany({daypoints: 0},(err,result)=>{
+            if (err) {
+                console.log(err)
+              } 
+        })
+        // schedule the next variable reset
+        scheduleReset();
+    }, t);
+}
+
+scheduleReset();
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
